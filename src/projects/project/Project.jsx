e80ac8a26371c5b8be0d8210f2bbdfd0e9a01736
Katrin3536 <Ekaterina_dr@tut.by>
2022-08-20 01:23:54 +0300

    added new feature

--- src/projects/project/Project.jsx
+++ src/projects/project/Project.jsx
@@ -7,7 +7,7 @@ function Project(props) {
         !props.hideButton
             ? <div className={styles.project}>
                 <div className={styles.icon} style={props.style}>
-                    <a className={styles.iconButton} href={props.href}>VIEW PROJECT</a>
+                    <a className={styles.iconButton} href={props.href} target={'_blank'}>VIEW PROJECT</a>
                 </div>
                 <div className={styles.projectInfo}>
                     <h4 className={styles.titleProject}>{props.title}</h4>
