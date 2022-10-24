ee508ef7748da60ecdf8db69ac27b26c09610587
Katrin3536 <Ekaterina_dr@tut.by>
2022-10-24 23:04:35 +0300

    added new project

--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -7,7 +7,7 @@ import socialNetImage from "../assets/image/Social-network.webp"
 import todoImage from "../assets/image/todolist.jpg"
 import counterImage from "../assets/image/counter.webp"
 import cardsImage from "../assets/image/cards.jpg"
-import futureImage from "../assets/image/future.jpg"
+import carImage from "../assets/image/rolls.png"
 
 
 function Projects() {
@@ -23,38 +23,37 @@ function Projects() {
     const cards = {
         backgroundImage: `url(${cardsImage})`,
     };
-    const future = {
-        backgroundImage: `url(${futureImage})`,
+    const car = {
+        backgroundImage: `url(${carImage})`,
+        backgroundSize: 'contain'
     };
-    return (
-        <div id={'projects'} className={styles.projectsBlock}>
-            <div className={`${styleContainer.container} ${styles.projectsContainer}`}>
-                <Title text={'MY PROJECTS'}/>
-                <div className={styles.projects}>
-                    <Project style={socialNetwork} title={"Social Network"}
-                             description={"TS, Redux, RestAPI, Thunk, ANT Design, Unit-Tests"}
-                             href={'https://katrin3536.github.io/samurai-way-main'}
-                             hideButton={false}/>
-                    <Project style={todolist} title={"ToDoList"}
-                             description={"TS, Redux, RestAPI, Thunk,Material UI, Unit-Tests"}
-                             href={'https://katrin3536.github.io/todolist'}
-                             hideButton={false}/>
-                    <Project style={counter} title={"Counter"}
-                             description={"My first project with React /Redux /TS etc"}
-                             href={'https://katrin3536.github.io/counters'}
-                             hideButton={false}/>
-                    <Project style={cards} title={"Training cards"}
-                             description={"It was a command work. App for learning some cards"}
-                             href={'https://katrin3536.github.io/cards'}
-                             hideButton={false}/>
-                    <Project style={future} title={"Future project"} description={"In progress"}
-                             hideButton={true}/>
-                    <Project style={future} title={"Future project"} description={"In progress"}
-                             hideButton={true}/>
-                </div>
+    return (<div id={'projects'} className={styles.projectsBlock}>
+        <div className={`${styleContainer.container} ${styles.projectsContainer}`}>
+            <Title text={'MY PROJECTS'}/>
+            <div className={styles.projects}>
+                <Project style={socialNetwork} title={"Social Network"}
+                         description={"TS, Redux, RestAPI, Thunk, ANT Design, Unit-Tests"}
+                         href={'https://katrin3536.github.io/samurai-way-main'}
+                         hideButton={false}/>
+                <Project style={todolist} title={"ToDoList"}
+                         description={"TS, Redux, RestAPI, Thunk,Material UI, Unit-Tests"}
+                         href={'https://katrin3536.github.io/todolist'}
+                         hideButton={false}/>
+                <Project style={counter} title={"Counter"}
+                         description={"My first project with React /Redux /TS etc"}
+                         href={'https://katrin3536.github.io/counters'}
+                         hideButton={false}/>
+                <Project style={cards} title={"Training cards"}
+                         description={"It was a command work. App for learning some cards"}
+                         href={'https://katrin3536.github.io/cards'}
+                         hideButton={false}/>
+                <Project style={car} title={"Сar rental service"}
+                         description={"Luxury and sport rental service with Angular"}
+                         href={'https://katrin3536.github.io/rent-cars-service'}
+                         hideButton={false}/>
             </div>
         </div>
-    );
+    </div>);
 }
 
 export default Projects;
